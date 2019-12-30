ce543abcab7cc1e999da90fb052e6dcee3b0e244
Alice Freda <alicefreda@gmail.com>
2019-12-30 23:48:51 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,5 +1,11 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
+    create_table :movies do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
     create_table :shows do |t|
       t.string :name
       t.string :network
