32bb74573f9c955a962344f617cdfa4af66cc97d
Alice Freda <alicefreda@gmail.com>
2019-12-30 23:46:30 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,6 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    add_column :shows do |t|
-      t.string :season
+    add_column :shows, :seaon, :string
     end
   end
 end
