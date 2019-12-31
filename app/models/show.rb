9d306bcd2af8727f1d0d8135f770965539dd63fa
Alice Freda <alicefreda@gmail.com>
2019-12-31 00:00:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,10 @@
 class Show < ActiveRecord::Base
+
   def self.highest_rating
     self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating)
   end
 end
